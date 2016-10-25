8ef937533f4cc53975e3d0e0d76675107b4ac791
micahwierenga <wierenga.micah@gmail.com>
2016-10-25 14:01:00 -0600

    created cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,12 +5,28 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardOne === cardTwo) {
-	alert('You found a match!');
+// if (cardOne === cardTwo) {
+// 	alert('You found a match!');
+// }
+// else if (cardThree === cardFour) {
+// 	alert('You found a match!');
+// }
+// else {
+// 	alert ('Sorry, try again!')
+// }
+
+
+//Finds the #board div
+var gameBoard = document.getElementById('#game-board');
+
+
+//Creates cards
+var createCards = function () {
+	for (var i = 0; i < 4; i++) {
+		var newCard = document.createElement('div');
+		newCard.className = 'card';
+		document.getElementsByClassName('board')[0].appendChild(newCard);
+	}
 }
-else if (cardThree === cardFour) {
-	alert('You found a match!');
-}
-else {
-	alert ('Sorry, try again!')
-}
+
+createCards();
